6f147f020132923c15a26705f0bbc5678fe28e62
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-02-15 09:53:40 +0800

    edited app.js

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ app.post("/failure", function(req,res){
 });
 
 //change port when depolying
-app.listen(process.env.PORT, function() {
+app.listen(3000, function() {
   console.log("Server is running on port 3000");
 });
